353b964cd132bf819cbf9d9c3b02b891a5e38505
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2024-04-22 10:46:30 -0300

    criando a seção de projetos e o componente de Lista dos projetos

--- src/containers/Projetos/index.tsx
+++ src/containers/Projetos/index.tsx
@@ -1,8 +1,18 @@
+import Projeto from '../../components/ListaProjetos'
 import Titulo from '../../components/Titulo'
+import { Lista } from './styles'
 
 const Projetos = () => (
   <section>
     <Titulo fontSize={16}>Projetos</Titulo>
+    <Lista>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+    </Lista>
   </section>
 )
 
--- src/styles.ts
+++ src/styles.ts
@@ -5,6 +5,8 @@ const EstiloGlobal = createGlobalStyle`
     margin: 0;
     padding: 0;
     font-family: "Inter", sans-serif;
+    text-decoration: none;
+    list-style: none;
   }
 
   body{
